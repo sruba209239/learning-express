6fb789306f76a37a25d0394c1daa648d6bb86a39
sruba mohapatra <sruba209239@gmail.com>
2023-01-06 02:19:09 +0530

    added test case for delele & get id

--- app.test.js
+++ app.test.js
@@ -19,40 +19,89 @@ DELETE => delete a Todo if its present, [204, 404]
 */
 describe("Todos API", () => {
   it("GET /todos => [Todos]", () => {
-    return request(app)
-      .get("/todos")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((response) => {
-        expect(response.body).toEqual(
-          expect.arrayContaining([
-            {
-              id: expect.any(Number),
-              name: expect.any(String),
-              done: expect.any(Boolean),
-            },
-          ])
-        );
-      });
+    return (
+      request(app)
+        .get("/todos")
+        // .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body).toEqual(
+            expect.arrayContaining([
+              {
+                id: expect.any(Number),
+                name: expect.any(String),
+                targetDate: expect.any(String), // check date later
+                done: expect.any(Boolean),
+              },
+            ])
+          );
+        })
+    );
   });
 
-  //   it("POST /todos => 201", () => {
-  //     return request(app)
-  //       .post("/todos")
-  //       .set("Accept", "application/json")
-  //       .expect("Content-Type", /json/)
-  //       .expect(200)
-  //       .then((response) => {
-  //         expect(response.body).toEqual(
-  //           expect.arrayContaining([
-  //             {
-  //               id: expect.any(Number),
-  //               name: expect.any(String),
-  //               done: expect.any(Boolean),
-  //             },
-  //           ])
-  //         );
-  //       });
-  //   });
+  it("GET /todos/{id} => Todo", () => {
+    return (
+      request(app)
+        .get("/todos/123")
+        // .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body).toEqual(
+            expect.objectContaining({
+              id: 123,
+              name: expect.any(String),
+              targetDate: expect.any(String), // check date later
+              done: expect.any(Boolean),
+            })
+          );
+        })
+    );
+  });
+
+  it("GET /todos/{id} => 404", () => {
+    return (
+      request(app)
+        .get("/todos/1234")
+        // .set("Accept", "application/json")
+        // .expect("Content-Type", /json/)
+        .expect(404)
+    );
+  });
+
+  it("POST /todos => create a todo", () => {
+    return (
+      request(app)
+        .post("/todos")
+        .send({
+          id: 121,
+          name: "watch avatar 2",
+          targetDate: new Date(),
+          done: true,
+        })
+        // .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect("Location", "/121")
+        .expect(201)
+        .then((response) => {
+          expect(response.header["Location"]).not.toBeNull();
+          expect(response.body).toEqual(
+            expect.objectContaining({
+              id: expect.any(Number),
+              name: expect.any(String),
+              targetDate: expect.any(String),
+              done: expect.any(Boolean),
+            })
+          );
+        })
+    );
+  });
+
+  it("DELETE /todos/{id} => Todo Deleted!", () => {
+    return request(app).delete("/todos/123").expect(204);
+  });
+  it("DELETE /todos/{id} => Todo not found", () => {
+    return request(app).delete("/todos/1234").expect(404);
+  });
 });
--- routes/todos.js
+++ routes/todos.js
@@ -1,16 +1,56 @@
 var express = require("express");
 var router = express.Router();
+var createError = require("http-errors");
 
+const todos = [{ id: 123, name: "helloo", targetDate: new Date(), done: true }];
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   // res.send("respond with a resource");
+  // connect to some db to retrieve the data
   res.json([
     {
       id: 123,
       name: "name 1",
+      targetDate: new Date(),
       done: false,
     },
   ]);
+});
+
+router.get("/:id", function (req, res, next) {
+  // res.send("respond with a resource");
+  // connect to some db to retrieve the data
+  const result = todos.find((todo) => todo.id === Number(req.params.id));
+
+  if (!result) {
+    return next(createError(404, "Todo Not Found!"));
+  }
+  res.json(result);
+});
+
+// Post - create a new todo
+router.post("/", function (req, res, next) {
+  res
+    .status(201)
+    .header("Location", req.url + req.body.id)
+    .json({
+      id: 121,
+      name: "watch avatar 2",
+      targetDate: new Date(),
+      done: true,
+    });
+});
+
+router.delete("/:id", function (req, res, next) {
+  const result = todos.find((todo) => todo.id === +req.params.id);
+  if (!result) {
+    return next(
+      createError(404, "No todo found to delete", {
+        error: "No todo found to delete",
+      })
+    );
+  }
+  res.status(204);
 });
 
 module.exports = router;
